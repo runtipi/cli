3502ffac8fb3f0ee643fca935abb0c90f13e8b34
Nicolas Meienberger <github@thisprops.com>
2025-05-16 22:52:27 +0200

    feat(go-cli): support symlinks in archive extract

--- go-cli/internal/utils/archive.go
+++ go-cli/internal/utils/archive.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func ExtractTarGz(archivePath, destDir string) error {
@@ -24,6 +25,11 @@ func ExtractTarGz(archivePath, destDir string) error {
 
 	tarReader := tar.NewReader(gzipReader)
 
+	absDestDir, err := filepath.Abs(destDir)
+	if err != nil {
+		return fmt.Errorf("failed to get absolute path of destination directory: %v", err)
+	}
+
 	for {
 		header, err := tarReader.Next()
 		if err == io.EOF {
@@ -34,11 +40,27 @@ func ExtractTarGz(archivePath, destDir string) error {
 		}
 
 		target := filepath.Join(destDir, header.Name)
+		absTarget, err := filepath.Abs(target)
+		if err != nil {
+			return fmt.Errorf("failed to get absolute path of target: %v", err)
+		}
+
+		if !strings.HasPrefix(absTarget, absDestDir) {
+			return fmt.Errorf("path traversal attack detected: %s attempts to escape to %s", header.Name, absTarget)
+		}
 
 		switch header.Typeflag {
 		case tar.TypeDir:
 			if err := os.MkdirAll(target, 0755); err != nil {
 				return fmt.Errorf("failed to create directory: %v", err)
+			}
+		case tar.TypeSymlink:
+			if err := os.MkdirAll(filepath.Dir(target), 0755); err != nil {
+				return fmt.Errorf("failed to create directory: %v", err)
+			}
+
+			if err := os.Symlink(header.Linkname, target); err != nil {
+				return fmt.Errorf("failed to create symbolic link: %v", err)
 			}
 		case tar.TypeReg:
 			if err := os.MkdirAll(filepath.Dir(target), 0755); err != nil {
