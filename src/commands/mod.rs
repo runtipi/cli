87c720eed293b901526e094a87a1accafe62f5d1
Nicolas Meienberger <github@thisprops.com>
2025-05-18 10:32:32 +0200

    chore: move go cli to root and remove rust cli

--- src/commands/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod app;
-pub mod debug;
-pub mod reset_password;
-pub mod start;
-pub mod stop;
-pub mod update;
-pub mod installed;
-pub mod version;
--- src/utils/constants.rs
+++
@@ -1,8 +0,0 @@
-pub const DEFAULT_NGINX_PORT: &str = "80";
-pub const DEFAULT_NGINX_PORT_SSL: &str = "443";
-pub const DEFAULT_POSTGRES_PORT: &str = "5432";
-pub const DEFAULT_DOMAIN: &str = "example.com";
-pub const DEFAULT_LOCAL_DOMAIN: &str = "tipi.local";
-pub const DOCKER_COMPOSE_YML: &str = include_str!("../assets/docker-compose.yml");
-pub const VERSION: &str = include_str!("../assets/VERSION");
-pub const DEFAULT_FORWARD_AUTH_URL: &str = "http://runtipi:3000/api/auth/traefik";
