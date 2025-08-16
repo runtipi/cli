bfbf21b59b90faa99f32f0cf02d75e334ec496f4
Nicolas Meienberger <github@thisprops.com>
2025-08-16 11:18:52 +0200

    fix: default to env ROOT_FOLDER_HOST

--- cmd/runtipi/main.go
+++ cmd/runtipi/main.go
@@ -19,10 +19,15 @@ var (
 
 func init() {
 	var err error
-	config.RootFolder, err = os.Getwd()
-	if err != nil {
-		fmt.Println("Error getting working directory:", err)
-		os.Exit(1)
+
+	config.RootFolder = os.Getenv("ROOT_FOLDER_HOST")
+
+	if config.RootFolder == "" {
+		config.RootFolder, err = os.Getwd()
+		if err != nil {
+			fmt.Println("Error getting working directory:", err)
+			os.Exit(1)
+		}
 	}
 
 	if envRootFolder := os.Getenv("ROOT_FOLDER_HOST"); envRootFolder != "" {
