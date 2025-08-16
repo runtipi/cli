640b04394eea6757a4d5126877d6286ade96c755
Nicolas Meienberger <github@thisprops.com>
2025-08-16 11:39:11 +0200

    fix: default root folder set to where the binary is instead of current directory

--- cmd/runtipi/main.go
+++ cmd/runtipi/main.go
@@ -22,9 +22,23 @@ var (
 func init() {
 	var err error
 
-	config.RootFolder, err = os.Getwd()
+	binaryPath, err := os.Executable()
 	if err != nil {
-		fmt.Println("Error getting working directory:", err)
+		fmt.Println("Error getting executable path:", err)
+		os.Exit(1)
+	}
+
+	evalPath, err := filepath.EvalSymlinks(binaryPath)
+	if err != nil {
+		fmt.Println("Error getting binary directory:", err)
+		os.Exit(1)
+	}
+
+	executableDir := filepath.Dir(evalPath)
+	config.RootFolder, err = filepath.Abs(executableDir)
+
+	if err != nil {
+		fmt.Println("Error getting absolute path for root folder:", err)
 		os.Exit(1)
 	}
 
